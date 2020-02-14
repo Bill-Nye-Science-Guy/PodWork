d8131cd0e55a974800e18366b1ac0bc3886a489a
Kate McKinney <kate.mckinney210@gmail.com>
2020-02-13 19:37:43 -0600

    more work

--- Week 1/Conditionals-Loops.js
+++ Week 1/Conditionals-Loops.js
@@ -1,14 +1,25 @@
-//PROBLEM 1 --> https://edabit.com/challenge/4gzDuDkompAqujpRi
+//TODO: PROBLEM 1 --> https://edabit.com/challenge/4gzDuDkompAqujpRi
+
 // Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
 
+function ProblemOne(x) {
+    var answer = 0;
+    for (var y = 1; y <= x; y+=1) {
+        console.log(answer += y);
+    }
+    return answer;
+}
+
+
+// console.log(ProblemOne(5)); //Should be: 15
+// console.log(ProblemOne(3)); //Should be: 6
+console.log(ProblemOne(-3)); //Should be: -6
 
 
 
+//TODO: PROBLEM 2 --> https://edabit.com/challenge/XnZAcvr4tCD9ppyrN
 
-
-//PROBLEM 2 --> https://edabit.com/challenge/XnZAcvr4tCD9ppyrN
 // Given a number, return a string of the word "Boom", which varies in the following ways:
-//
 //     The string should include n number of "o"s, unless n is below 2 (in that case, return "boom").
 // If n is evenly divisible by 2, add an exclamation mark to the end.
 //     If n is evenly divisible by 5, return the string in ALL CAPS.
@@ -31,7 +42,8 @@
 
 
 
-//PROBLEM 3 --> https://edabit.com/challenge/S7oxczk4Rtu99kaZ8
+//TODO: PROBLEM 3 --> https://edabit.com/challenge/S7oxczk4Rtu99kaZ8
+
 // A game of table tennis almost always sounds like Ping! followed by Pong! Therefore, you know that Player 2 has won if you hear Pong! as the last sound (since Player 1 didn't return the ball back).
 //
 // Given an array of Ping!, create a function that inserts Pong! in between each element. Also:
