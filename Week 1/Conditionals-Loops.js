88528aa7e18f2ac7f0c6b559985fc3c5dc535525
Kate McKinney <kate.mckinney210@gmail.com>
2020-02-17 18:07:48 -0600

    finished this week's problems

--- Week 1/Conditionals-Loops.js
+++ Week 1/Conditionals-Loops.js
@@ -4,28 +4,27 @@
 
 function ProblemOne(x) {
     var answer = 0;
+    // For positives...
     if (x < 0) {
         x = (x * -1);
         for (var y = 1; y <= x; y+=1) {
-            console.log(answer += y);
+            answer += y;
         }
         return (answer * -1);
     }
+    // And for negatives...
     else {
         for (var y = 1; y <= x; y+=1) {
-            console.log(answer += y);
+            answer += y;
         }
         return answer;
     }
-
-
 }
-
 
 // console.log(ProblemOne(5)); //Should be: 15
 // console.log(ProblemOne(3)); //Should be: 6
 // console.log(ProblemOne(-3)); //Should be: -6
-console.log(ProblemOne(-5)); //Should be: -15
+// console.log(ProblemOne(-5)); //Should be: -15
 
 
 
@@ -36,35 +35,66 @@ console.log(ProblemOne(-5)); //Should be: -15
 // If n is evenly divisible by 2, add an exclamation mark to the end.
 //     If n is evenly divisible by 5, return the string in ALL CAPS.
 //     If n is evenly divisible by both 2 and 5, return the string in ALL CAPS and add an exclamation mark to the end.
-//Examples:
-// boomIntensity(4) ➞ "Boooom!"
-// // There are 4 "o"s and 4 is divisible by 2 (exclamation mark included)
-//
-// boomIntensity(1) ➞ "boom"
-// // 1 is lower than 2, so we return "boom"
-//
-// boomIntensity(5) ➞ "BOOOOOM"
-// // There are 5 "o"s and 5 is divisible by 5 (all caps)
-//
-// boomIntensity(10) ➞ "BOOOOOOOOOOM!"
-// // There are 10 "o"s and 10 is divisible by 2 and 5 (all caps and exclamation mark included)
 
+function DaBoom(x) {
+    var Boom;
+    if (x > 2) {
+        if (x % 10 === 0) {
+            Boom = ("B" + "o".repeat(x) + "m" + "!").toUpperCase();
+        }
+        else if (x % 2 === 0) {
+            Boom = ("B" + "o".repeat(x) + "m" + "!");
+        }
+        else if (x % 5 === 0) {
+            Boom = ("B" + "o".repeat(x) + "m").toUpperCase();
+        }
+        else {
+            Boom = "B" + "o".repeat(x) + "m"
+        }
+    }
+    else{
+        Boom = "Boom";
+    }
+    return Boom;
+}
 
-
-
+// console.log(DaBoom(1));
+// console.log(DaBoom(3));
+// console.log(DaBoom(4));
+// console.log(DaBoom(5));
+// console.log(DaBoom(10));
 
 
 //TODO: PROBLEM 3 --> https://edabit.com/challenge/S7oxczk4Rtu99kaZ8
 
-// A game of table tennis almost always sounds like Ping! followed by Pong! Therefore, you know that Player 2 has won if you hear Pong! as the last sound (since Player 1 didn't return the ball back).
-//
+// A game of table tennis almost always sounds like Ping! followed by Pong! Therefore, you know that Player 2 has won if you hear Pong! as the last sound.
 // Given an array of Ping!, create a function that inserts Pong! in between each element. Also:
-//
-// If win equals true, end the list with Pong!.
-// If win equals false, end with Ping! instead.
-//     Examples
-// pingPong(["Ping!"], true) ➞ ["Ping!", "Pong!"]
-//
-// pingPong(["Ping!", "Ping!"], false) ➞ ["Ping!", "Pong!", "Ping!"]
-//
-// pingPong(["Ping!", "Ping!", "Ping!"], true) ➞ ["Ping!", "Pong!", "Ping!", "Pong!", "Ping!", "Pong!"]
+    // If win equals true, end the list with Pong!.
+    // If win equals false, end with Ping! instead.
+
+firstArray = ["Ping!"];
+secondArray = ["Ping!", "Ping!"];
+thirdArray = ["Ping!", "Ping!", "Ping!"];
+
+// y = true means Pong wins, y = false means Ping wins
+
+function PingPong(x, y) {
+   var Result;
+    if (y === true) {
+        x = (x.join(",Pong!,").split(","));
+        x.push("Pong!");
+        Result = x;
+    }
+    else {
+        x = (x.join(",Pong!,").split(","));
+        Result = x;
+    }
+  return Result;
+}
+
+// console.log(PingPong(firstArray, true));
+// console.log(PingPong(firstArray, false));
+// console.log(PingPong(secondArray, true));
+// console.log(PingPong(secondArray, false));
+// console.log(PingPong(thirdArray, true));
+// console.log(PingPong(thirdArray, false));
