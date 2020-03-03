4e425ecf58b02619d781634c2af750ae1c089a59
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-03 13:33:04 -0600

    adding stuff

--- Week 3/jQuery.js
+++ Week 3/jQuery.js
@@ -5,3 +5,44 @@
 //     The left frame swaps to the right and takes the image from the frame in the center.
 //     The center frame swaps randomly to either the left or right.
 //     The right frame swaps to the left and takes the image from the frame in the center.
+
+
+$("#TheButton").click(function () {
+    $("dd").toggleClass("invisible");
+});
+
+$("#The2ndButton").click(function () {
+    return $("ul").each(function () {
+        return $(this).children().last().css("background", "yellow");
+    });
+});
+
+// BOLDS TEXT WHEN PARENT H3 IS CLICKED
+// $("h3").click(function () {
+//     return $(this).next().children().css("font-weight", "bold");
+// });
+
+$("li").click(function () {
+    return $(this).parent().children().first().css("color", "blue");
+});
+
+
+$(".picbutts").click(function () {
+    var clickedButton = $(this).attr("id");
+    if (clickedButton === "pic1butt"){
+        $("#pic1").html($("#pic2").children());
+        // $("#pic2").html($("#pic3").children());
+        // $("#pic3").html($("#pic1").children());
+    }
+    if (clickedButton === "pic2butt"){
+
+    }
+    if (clickedButton === "pic3butt"){
+
+    }
+
+    // '<img src="images/Jimbo.JPG" alt="Jimbo">'
+    // '<img src="images/JokeCat.JPG" alt="Joking Cat">'
+    // '<img src="images/WrinkleCat.jpg" alt="Wrinkle Cat">'
+});
+
