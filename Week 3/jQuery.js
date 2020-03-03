a32262d13ccb612baaf79bd5955bc9b854feadf1
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-03 12:29:43 -0600

    more stuff

---
+++ Week 3/jQuery.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Create 3 divs that each look like a picture frame. Each one should have a unique background image and a button underneath that swaps the image from the frame. Use the traversing functions of jQuery to accomplish this.
+//
+//     The left frame swaps to the right and takes the image from the frame in the center.
+//     The center frame swaps randomly to either the left or right.
+//     The right frame swaps to the left and takes the image from the frame in the center.
