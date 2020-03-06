18bf12ab031882948a047909766dd3ee83b4ece0
Kate McKinney <kate.mckinney210@gmail.com>
2020-03-06 12:01:46 -0600

    done with w3 pod work

--- Week 3/jQuery.js
+++ Week 3/jQuery.js
@@ -1,48 +1,20 @@
 "use strict";
 
-// Create 3 divs that each look like a picture frame. Each one should have a unique background image and a button underneath that swaps the image from the frame. Use the traversing functions of jQuery to accomplish this.
-//
-//     The left frame swaps to the right and takes the image from the frame in the center.
-//     The center frame swaps randomly to either the left or right.
-//     The right frame swaps to the left and takes the image from the frame in the center.
+//#1. Create a paragraph element with some text and append it to the end of the document body using jQuery. (#3 on https://www.w3resource.com/jquery-exercises/1/index.php#EDITOR)
+
+    $("body").append("<p>" + "<span>" + "Words and stuff" + "</span>" + "</p>");
+
+//#2. Using jQuery add the class " w3r_font_color " to the last paragraph element. (#5 on https://www.w3resource.com/jquery-exercises/1/index.php#EDITOR)
 
 
-$("#TheButton").click(function () {
-    $("dd").toggleClass("invisible");
-});
-
-$("#The2ndButton").click(function () {
-    return $("ul").each(function () {
-        return $(this).children().last().css("background", "yellow");
+    $("button").click(function () {
+        $("p:last").addClass("w3r_font_color");
+        countElements();
     });
-});
 
-// BOLDS TEXT WHEN PARENT H3 IS CLICKED
-// $("h3").click(function () {
-//     return $(this).next().children().css("font-weight", "bold");
-// });
+//#3. Using jQuery count every element (including head, body, etc.) in the document. (#17 on https://www.w3resource.com/jquery-exercises/1/index.php#EDITOR)
 
-$("li").click(function () {
-    return $(this).parent().children().first().css("color", "blue");
-});
-
-
-$(".picbutts").click(function () {
-    var clickedButton = $(this).attr("id");
-    if (clickedButton === "pic1butt"){
-        $("#pic1").html($("#pic2").children());
-        // $("#pic2").html($("#pic3").children());
-        // $("#pic3").html($("#pic1").children());
+    function countElements() {
+        var theCount = $("*").css("padding", "1px").length;
+        console.log("There are " + theCount + " elements");
     }
-    if (clickedButton === "pic2butt"){
-
-    }
-    if (clickedButton === "pic3butt"){
-
-    }
-
-    // '<img src="images/Jimbo.JPG" alt="Jimbo">'
-    // '<img src="images/JokeCat.JPG" alt="Joking Cat">'
-    // '<img src="images/WrinkleCat.jpg" alt="Wrinkle Cat">'
-});
-
